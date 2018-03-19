6480d7ef2486d86473065ecf7557dd4fa5c59557
Jose Alejandro Matute Flores <matutefl@CHESTBURSTER.uni-muenster.de>
2018-03-19 15:39:04 +0100

    added a printout to get vowel measurement

--- FAVE-extract/bin/extractFormants.py
+++ FAVE-extract/bin/extractFormants.py
@@ -890,8 +890,9 @@ def getVowelMeasurement(vowelFileStem, p, w, speechSoftware, formantPredictionMe
             LPCs = []
             nFormants = 3
             while nFormants <= 6:
-                os.system(os.path.join(PRAATPATH, PRAATNAME) + ' ' + os.path.join(SCRIPTS_HOME, 'extractFormants.praat') + ' ' +
-                          vowelWavFile + ' ' + str(nFormants) + ' ' + str(maxFormant) + ' ' ' ' + str(windowSize) + ' ' + str(preEmphasis) + ' burg')
+                command =os.path.join(PRAATPATH, PRAATNAME) + ' ' + os.path.join(SCRIPTS_HOME, 'extractFormants.praat') + ' ' + vowelWavFile + ' ' + str(nFormants) + ' ' + str(maxFormant) + ' ' ' ' + str(windowSize) + ' ' + str(preEmphasis) + ' burg'
+                print "Command Used for PRAAT with ", nFormants, command
+                os.system(command)
                 lpc = praat.Formant()
                 lpc.read(os.path.join(SCRIPTS_HOME, vowelFileStem + '.Formant'))
                 LPCs.append(lpc)
